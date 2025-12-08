29669628d8e28d756db7c06178217826cc548693
samihns1 <samihs@uchicago.edu>
2025-12-08 23:47:35 +0000

    Major formating changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,61 +7,6 @@ Rails.application.routes.draw do
 
   get("/rules", { :controller => "home", :action => "rules"})
 
-  # Routes for the Invitation resource:
-
-  # CREATE
-  post("/insert_invitation", { :controller => "invitations", :action => "create" })
-
-  # READ
-  get("/invitations", { :controller => "invitations", :action => "index" })
-
-  get("/invitations/:path_id", { :controller => "invitations", :action => "show" })
-
-  # UPDATE
-
-  post("/modify_invitation/:path_id", { :controller => "invitations", :action => "update" })
-
-  # DELETE
-  get("/delete_invitation/:path_id", { :controller => "invitations", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Move resource:
-
-  # CREATE
-  post("/insert_move", { :controller => "moves", :action => "create" })
-
-  # READ
-  get("/moves", { :controller => "moves", :action => "index" })
-
-  get("/moves/:path_id", { :controller => "moves", :action => "show" })
-
-  # UPDATE
-
-  post("/modify_move/:path_id", { :controller => "moves", :action => "update" })
-
-  # DELETE
-  get("/delete_move/:path_id", { :controller => "moves", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Gameplayer resource:
-
-  # CREATE
-  post("/insert_gameplayer", { :controller => "gameplayers", :action => "create" })
-
-  # READ
-  get("/gameplayers", { :controller => "gameplayers", :action => "index" })
-
-  get("/gameplayers/:path_id", { :controller => "gameplayers", :action => "show" })
-
-  # UPDATE
-
-  post("/modify_gameplayer/:path_id", { :controller => "gameplayers", :action => "update" })
-
-  # DELETE
-  get("/delete_gameplayer/:path_id", { :controller => "gameplayers", :action => "destroy" })
-
   #------------------------------
 
   # Routes for the Game resource:
